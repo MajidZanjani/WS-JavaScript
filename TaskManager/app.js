b420955e53e3813e3552bc9e04626ef7b0bcc7fb
MajidZanjani <majidmmz1972@gmail.com>
2023-11-16 13:24:50 +0400

    commit

--- TaskManager/app.js
+++ TaskManager/app.js
@@ -7,7 +7,14 @@ const updateBtn = document.getElementById('update');
 let task = {};
 let taskId;
 
-// localStorage.clear();
+function appRestart() {
+  const restartConfirm = confirm('Do you realy want to clear all the tasks?!');
+  if (restartConfirm) {
+    localStorage.clear();
+    location.reload();
+  }
+}
+
 // console.log(localStorage);
 
 function taskHTML(task) {
@@ -18,32 +25,30 @@ function taskHTML(task) {
     `;
 }
 
-if (localStorage.lengh != 0) {
-  Object.keys(localStorage).forEach((key) => {
-    task = JSON.parse(localStorage.getItem(key));
-    addTask(task);
-  });
-}
-
-taskCheck = (task) => {
-  return task.taskTitle !== '' && task.taskDesc !== '' && task.dueDate !== '';
-};
-
 function clearForm() {
   taskTitleField.value = '';
   taskDescField.value = '';
   dueDateField.value = '';
+  taskTitleField.focus();
 }
 
 function addTask(task) {
-  let newTask = document.createElement('li');
+  const newTask = document.createElement('li');
   newTask.setAttribute('id', task.taskId);
   newTask.setAttribute('onclick', `editTask(${task.taskId})`);
   newTask.classList.add('task-item'); // Add a class for styling
   // Using innerHTML to structure the task item with labeled descriptions and increased spacing
-
   newTask.innerHTML = taskHTML(task);
   taskList.appendChild(newTask);
+}
+
+function updateTask(taskId, task) {
+  const editedLi = document.getElementById(taskId);
+  editedLi.innerHTML = taskHTML(task);
+  localStorage.setItem(taskId, JSON.stringify(task));
+  addBtn.classList.toggle('invisible');
+  updateBtn.classList.toggle('invisible');
+  clearForm();
 }
 
 const addTaskHandler = () => {
@@ -55,24 +60,10 @@ const addTaskHandler = () => {
   };
   if (taskCheck(task)) {
     addTask(task);
-    window.localStorage.setItem(task.taskId, JSON.stringify(task));
+    localStorage.setItem(task.taskId, JSON.stringify(task));
     clearForm();
   } else {
     alert("Please fill all the task's fields before storing it.");
-  }
-};
-
-const editTask = (editableTaskId) => {
-  let taskLi = document.getElementById(editableTaskId);
-  taskTitleField.value =
-    taskLi.getElementsByClassName('task-title')[0].innerText;
-  taskDescField.value = taskLi.getElementsByClassName('task-desc')[0].innerText;
-  dueDateField.value = taskLi.getElementsByClassName('task-date')[0].innerText;
-  taskId = editableTaskId;
-  if (addBtn.classList.contains('invisible')) {
-  } else {
-    addBtn.classList.toggle('invisible');
-    updateBtn.classList.toggle('invisible');
   }
 };
 
@@ -90,11 +81,29 @@ const updateTaskHandler = () => {
   }
 };
 
-function updateTask(taskId, task) {
-  const editedLi = document.getElementById(taskId);
-  editedLi.innerHTML = taskHTML(task);
-  window.localStorage.setItem(taskId, JSON.stringify(task));
-  addBtn.classList.toggle('invisible');
-  updateBtn.classList.toggle('invisible');
-  clearForm();
+const editTask = (editableTaskId) => {
+  const taskLi = document.getElementById(editableTaskId);
+  taskTitleField.value =
+    taskLi.getElementsByClassName('task-title')[0].innerText;
+  taskDescField.value = taskLi.getElementsByClassName('task-desc')[0].innerText;
+  dueDateField.value = taskLi.getElementsByClassName('task-date')[0].innerText;
+  taskId = editableTaskId;
+  if (addBtn.classList.contains('invisible')) {
+  } else {
+    addBtn.classList.toggle('invisible');
+    updateBtn.classList.toggle('invisible');
+  }
+};
+
+if (localStorage.lengh != 0) {
+  Object.keys(localStorage).forEach((key) => {
+    task = JSON.parse(localStorage.getItem(key));
+    addTask(task);
+  });
 }
+
+taskCheck = (task) => {
+  return task.taskTitle !== '' && task.taskDesc !== '' && task.dueDate !== '';
+};
+
+taskTitleField.focus();

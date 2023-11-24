c8e894b7db15f7ef7d691695771acd75a4a02cb7
MajidZanjani <majidmmz1972@gmail.com>
2023-11-24 21:48:56 +0400

    almost there!

--- taskmate/src/App.js
+++ taskmate/src/App.js
@@ -1,9 +1,21 @@
+import { useState } from 'react';
+import { Header } from './components/Header';
+import { AddTask } from './components/AddTask';
+import { Footer } from './components/Footer';
+import { TaskList } from './components/TaskList';
 import './App.css';
 
 function App() {
+  const [tasks, setTasks] = useState([]);
+
   return (
     <div className="App">
-      <h1>Hello World!</h1>
+      <Header />
+      <main>
+        <AddTask tasks={tasks} setTasks={setTasks} />
+        <TaskList tasks={tasks} setTasks={setTasks} />
+      </main>
+      <Footer />
     </div>
   );
 }

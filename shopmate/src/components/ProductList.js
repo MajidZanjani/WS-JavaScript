5d0f7852fba586246ac7a2a58939e24ae00e1ad3
MajidZanjani <majidmmz1972@gmail.com>
2023-11-25 17:01:25 +0400

    creating custom fetch -> useFetch

--- shopmate/src/components/ProductList.js
+++ shopmate/src/components/ProductList.js
@@ -1,19 +1,9 @@
-import { useCallback, useEffect, useState } from 'react';
+import { useState } from 'react';
+import { useFetch } from '../hook/useFetch';
 
 export const ProductList = () => {
-  const [products, setProducts] = useState([]);
   const [url, setUrl] = useState('http://localhost:8000/products');
-
-  // Because the "fetchProducts" is an outside function for "useEffect", we should use "useCallback" to keep "fetchProducts" cached and resolve the infinite loop due to the change of "fetchProducts" in every re-rendering.
-  const fetchProducts = useCallback(async () => {
-    const response = await fetch(url);
-    const data = await response.json();
-    setProducts(data);
-  }, [url]);
-
-  useEffect(() => {
-    fetchProducts();
-  }, [fetchProducts]);
+  const { data: products } = useFetch(url);
 
   return (
     <section>
@@ -27,18 +17,19 @@ export const ProductList = () => {
           In Stock Only
         </button>
       </div>
-      {products.map((product) => (
-        <div className="card" key={product.id}>
-          <p className="id">{product.id}</p>
-          <p className="name">{product.name}</p>
-          <p className="info">
-            <span>${product.price}</span>
-            <span className={product.in_stock ? 'instock' : 'unavailable'}>
-              {product.in_stock ? 'In Stock' : 'Unavailable'}
-            </span>
-          </p>
-        </div>
-      ))}
+      {products &&
+        products.map((product) => (
+          <div className="card" key={product.id}>
+            <p className="id">{product.id}</p>
+            <p className="name">{product.name}</p>
+            <p className="info">
+              <span>${product.price}</span>
+              <span className={product.in_stock ? 'instock' : 'unavailable'}>
+                {product.in_stock ? 'In Stock' : 'Unavailable'}
+              </span>
+            </p>
+          </div>
+        ))}
     </section>
   );
 };
